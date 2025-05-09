3f4ae01a7bdfcc752ae4b12ce4ec61c5aeb59353
Tasir Mahtab Haque <mahtabhaque2587@gmail.com>
2025-05-10 00:57:27 +0600

    blur

--- basic.py
+++ basic.py
@@ -7,4 +7,8 @@ cv.imshow('Cat', img)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
+# blurring an image
+blur = cv.GaussianBlur(img,(3,3), cv.BORDER_DEFAULT)
+cv.imshow('Blurred Image', blur)
+
 cv.waitKey(0)

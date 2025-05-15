6cd0f37cc8a96f07b6ae2d071514f0134f22f204
Tasir Mahtab Haque <mahtabhaque2587@gmail.com>
2025-05-15 22:08:58 +0600

    Refactor image processing steps in basic.py for clarity

--- basic.py
+++ basic.py
@@ -8,7 +8,11 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
 # blurring an image
-blur = cv.GaussianBlur(img,(3,3), cv.BORDER_DEFAULT)
+blur = cv.GaussianBlur(img,(7,7), cv.BORDER_DEFAULT)
 cv.imshow('Blurred Image', blur)
+
+# Edge Cascade
+canny = cv.Canny(img, 125, 175)
+cv.imshow('Canny edges', canny)
 
 cv.waitKey(0)

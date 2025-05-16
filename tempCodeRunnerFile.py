a3975bf85bc568d811332e41e9bb4cd303bf6638
Tasir Mahtab Haque <mahtabhaque2587@gmail.com>
2025-05-16 12:08:51 +0600

    Refactor translation and rotation functions in transformations.py for clarity and consistency

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+# Translation
+# def translate(img, x, y):
+#     transMat = np.float32([[1,0,x],[0,1,y]])
+#     dimensions = (img.shape[1], img.shape[0])
+#     return cv.warpAffine(img, transMat, dimensions)
+
+# # -x --> Left
+# # -y --> Up
+# # x --> Right
+# # y --> Down
+
+# translated = translate(img, -100, 100)
+# cv.imshow('Translated', translated)
--- transformations.py
+++ transformations.py
@@ -2,21 +2,37 @@ import cv2 as cv
 import numpy as np
 
 img = cv.imread('Photos/boston.png')
+cv.imshow('Boston', img)
 
-cv.imshow('Boston',img)
-
-# translation (shifting the image along the x and y axis)
+# Translation
 def translate(img, x, y):
     transMat = np.float32([[1,0,x],[0,1,y]])
     dimensions = (img.shape[1], img.shape[0])
     return cv.warpAffine(img, transMat, dimensions)
 
 # -x --> Left
-# -y --> up
+# -y --> Up
 # x --> Right
 # y --> Down
 
-translated = translate(img, 100, 100)
-cv.imshow('Translated Image', translated)
+translated = translate(img, -100, 100)
+cv.imshow('Translated', translated)
+
+# Rotation
+def rotate(img, angle, rotPoint=None):
+    (height,width) = img.shape[:2]
+
+    if rotPoint is None:
+        rotPoint = (width//2,height//2)
+    
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    dimensions = (width,height)
+
+    return cv.warpAffine(img, rotMat, dimensions)
+
+rotated = rotate(img, -45)
+cv.imshow('Rotated', rotated)
+
+
 
 cv.waitKey(0)
